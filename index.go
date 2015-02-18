3718d4e23bc6f5fdec2785c608e8b99087c81776
drKraken <kuchuklehjs@gmail.com>
2015-02-19 00:12:53 +0200

    modify index

--- index.go
+++ index.go
@@ -4,9 +4,32 @@ import (
 	"fmt"
 )
 
+// entry point of app
 func main() {
 	fmt.Println("Hello world!")
 	secFunc()
+	hello := "Hello world"
+	fmt.Println(len("Hello World!"))
+	fmt.Println("Hello World!")
+	fmt.Println(hello)
+	fmt.Println(true || false)
+
+	var x string = "hello"
+	var y string = "hello"
+
+	fmt.Println(x == y)
+
+	z := 10
+	z += 1
+	fmt.Println(z)
+
+	for i := 0; i < 5; i++ {
+		if i%2 == 0 {
+			fmt.Println("odd")
+		} else {
+			fmt.Println("even")
+		}
+	}
 }
 
 func secFunc() {
